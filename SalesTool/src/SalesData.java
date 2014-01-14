644eb348172e0746072ceb6a0eaf8b14852a5f06
cmiranda15 <christine.miranda@comcast.net>
2014-01-14 11:54:11 -0500

    First code check in practice yay

---
+++ SalesTool/src/SalesData.java
@@ -0,0 +1,12 @@
+
+public class SalesData {
+	
+	int[] data = {0,4,42};
+	
+	public void display() {
+		System.out.println("Data:");
+		for (int n: data) {
+			System.out.println("Next value: " + n);
+		}
+	}
+}
---
+++ SalesTool/src/myMain.java
@@ -0,0 +1,20 @@
+
+public class myMain {
+
+	public static void main(String[] args) {
+		System.out.println("Executing main");
+		displayGreeting();
+		SalesData data = new SalesData();
+		data.display();
+
+	}
+	
+	public static void displayGreeting() {
+		System.out.println("Hello Happy Sales People!");
+		System.out.println("This app shows sales data.");
+		System.out.println("-------------------------");
+		System.out.println("Test 1");
+		System.out.println("Test 2");
+	}
+
+}
